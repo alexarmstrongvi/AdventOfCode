ae98bd6705f2ca3068359e8e07470c8c9b706984
Alex Armstrong <alex.armstrong.vi@gmail.com>
2024-12-07 10:57:51 -0800

    2024 Day 7 - Improved performance with dynamic programming

--- 2024/day7/main.py
+++ 2024/day7/main.py
@@ -4,7 +4,7 @@ import logging
 import time
 from collections.abc import Callable, Iterable, Sequence
 from functools import reduce
-from itertools import product
+from itertools import product, starmap
 from math import floor, log10
 from operator import add, mul
 
@@ -20,14 +20,14 @@ def main() -> None:
     args = aoc.parse_args()
     aoc.configure_logging(args.log_level)
     text = aoc.read_input(args.input)
-    
+
     # Solution
     calibration_equations = [parse(line) for line in text.splitlines()]
 
     # Part 1
     start = time.perf_counter()
     total = sum(
-        test_val for test_val, nums in calibration_equations 
+        test_val for test_val, nums in calibration_equations
         if could_be_true(test_val, nums, ops = (add, mul))
     )
     elapsed = time.perf_counter() - start
@@ -36,34 +36,34 @@ def main() -> None:
     # Part 2
     start = time.perf_counter()
     total = sum(
-        test_val for test_val, nums in calibration_equations 
+        test_val for test_val, nums in calibration_equations
         if could_be_true(test_val, nums, ops = (add, mul, int_concat))
     )
     elapsed = time.perf_counter() - start
     print(f'Part 2: {total} [t={elapsed:.3f}sec]')
 
 ################################################################################
-BinaryIntOperator = Callable[[int, int], int]
+BinaryIntOp = Callable[[int, int], int]
 def could_be_true(
-    test_val: int, 
-    nums    : Sequence[int], 
-    ops     : Iterable[BinaryIntOperator],
+    test_val: int,
+    nums    : Sequence[int],
+    ops     : Iterable[BinaryIntOp],
 ) -> bool:
-    ops_permutations = product(ops, repeat=len(nums)-1)
+    # Option 1: Procedural - Brute Force
+    # ops_permutations = product(ops, repeat=len(nums)-1)
+    # for ops_permutation in ops_permutations:
+    #     total = nums[0]
+    #     for op, num in zip(ops_permutation, nums[1:]):
+    #         total = op(total,num)
+    #         # Early exit (negligable performance change)
+    #         # if total > test_val:
+    #         #     break
+    #     if total == test_val:
+    #         return True
+    # return False
 
-    # Option 1: Procedural
-    for ops_permutation in ops_permutations:
-        total = nums[0]
-        for op, num in zip(ops_permutation, nums[1:]):
-            total = op(total,num)
-            # Early exit (negligable performance change)
-            # if total > test_val:
-            #     break
-        if total == test_val:
-            return True
-    return False
-
-    # Option 2: Functional (notably slower)
+    # Option 2: Functional - Brute Force (2X slower than option 1)
+    # ops_permutations = product(ops, repeat=len(nums)-1)
     # produces_test_val = lambda ops_permutation : test_val == star_reduce(
     #     func     = lambda acc, op, num : op(acc, num),
     #     iterable = zip(ops_permutation, nums[1:]),
@@ -71,7 +71,29 @@ def could_be_true(
     # )
     # return any(map(produces_test_val, ops_permutations))
 
-def star_reduce(func, iterable, initial): 
+    # Option 3: Dynamic + Procedural (4-5X faster than option 1)
+    # results = {nums[0]}
+    # for n in nums[1:]:
+    #     intermediate_results = set()
+    #     for r, op in product(results, ops):
+    #         x = op(r, n)
+    #         # ASSUMPTION: All ops return a result larger than the inputs
+    #         if x <= test_val:
+    #             intermediate_results.add(x)
+    #     results = intermediate_results
+
+    # Option 4: Dynamic + Functional
+    apply_all_ops = lambda acc, num : (
+        # ASSUMPTION: All ops return a result larger than the inputs
+        filter(lambda x : x <= test_val,
+            starmap(lambda r, op : op(r, num),
+                product(acc, ops)
+    )))
+    results = set(reduce(apply_all_ops, nums[1:], {nums[0]}))
+
+    return test_val in results
+
+def star_reduce(func, iterable, initial):
     return reduce(lambda acc, args : func(acc, *args), iterable, initial)
 
 def int_concat(a: int, b: int) -> int:
