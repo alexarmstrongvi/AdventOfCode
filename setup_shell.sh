cb2bca4da28fc03b19925f18d29b01fc927aed13
Alex Armstrong <alex.armstrong.vi@gmail.com>
2025-01-04 13:33:26 -0800

    Fixed pyright LSP issues finding aoc_utils module
    Issue 1) LSPs are unable to find source for packages installed in
    editable mode unless package is the root dir or is added to the
    `extraPaths` configuration option in pyrightconfig.json
    Issue 2) Having an `aoc_utils` subdirectory in the pyright root dir that
    is not the intended python package was throwing it off.

--- setup_shell.sh
+++ setup_shell.sh
@@ -1,7 +1,7 @@
 export AOC_PATH="$(dirname $(realpath $0))"
 echo "AOC_Path = $AOC_PATH"
-alias C="$AOC_PATH/aoc_utils/c/build_and_run_c.sh"
-alias Cpp="$AOC_PATH/aoc_utils/cpp/build_and_run_cpp.sh"
+alias C="$AOC_PATH/lib/c/build_and_run_c.sh"
+alias Cpp="$AOC_PATH/lib/cpp/runcpp"
 # export MallocStackLogging=1
 # unset MallocStackLogging
 
