217aaed6cb346fbe2bba051465b52053cc090f0e
Alex Armstrong <alex.armstrong.vi@gmail.com>
2025-01-12 15:00:26 -0800

    2024 day1: refactored haskell solution

--- 2024/day01/main.hs
+++ 2024/day01/main.hs
@@ -1,8 +1,11 @@
 --------------------------------------------------------------------------------
 -- Day 01: Historian Hysteria
 --------------------------------------------------------------------------------
-import Data.List (transpose, sort)
-import System.CPUTime (getCPUTime, cpuTimePrecision)
+-- Standard library
+import Data.Foldable (traverse_)
+import Data.Function (on)
+import Data.List (sort)
+import Data.Maybe (listToMaybe)
 import System.Environment (getArgs)
 import System.IO (readFile)
 import Text.Printf (printf)
@@ -10,54 +13,75 @@ import Text.Read (readMaybe)
 import qualified Data.Map as Map
 
 --------------------------------------------------------------------------------
-day01 :: [(Int, Int)] -> IO ()
-day01 pairs = do
-    let precision = toMillsec cpuTimePrecision
-    -- Part 1
-    start <- getCPUTime
-    let (list1, list2) = unzip pairs
-    let distance x y   = abs (x - y)
-    let total          = sum $ zipWith distance (sort list1) (sort list2)
-    end <- getCPUTime
-    let elapsed = toMillsec (end - start)
-    printf "Part 1: %d [%.3f +/- %.0f ms]\n" total elapsed precision
+-- Solution
+type InputData = [(Int, Int)]
+type Solution = InputData -> IO ()
 
-    -- Part 2
-    start <- getCPUTime
-    let counts     = countOccurrences list2
-    let getCount x = Map.findWithDefault 0 x counts
-    let score      = sum $ zipWith (*) list1 (map getCount list1)
-    end <- getCPUTime
-    let elapsed = toMillsec (end - start)
-    printf "Part 2: %d [%.3f +/- %.0f ms]\n" score elapsed precision
-
-countOccurrences :: (Ord k) => [k] -> Map.Map k Int
-countOccurrences = foldr (\x -> Map.insertWith (+) x 1) Map.empty
-
---------------------------------------------------------------------------------
-toMillsec :: Integer -> Double
-toMillsec time = fromIntegral time / 1e9 -- getCPUTime returns picoseconds
-
---------------------------------------------------------------------------------
 main :: IO ()
-main = do
-    args <- getArgs
-    case args of
-        [fileName] -> do
-            text <- readFile fileName
-            case parseText text of
-                Just pairs -> do day01 pairs
-                _ -> putStrLn "ERROR | Failed to parse inputs"
-        _ -> putStrLn "Usage: ColumnSum <input file>"
+main = runSolution day01
 
+day01 :: Solution
+day01 input = do
+    let total = part1 input
+    printf "Part 1: %d\n" total
+    let score = part2 input
+    printf "Part 2: %d\n" score
+
+part1    = uncurry getTotal . unzip
+getTotal = sum ... zipWith distance `on` sort
+distance = abs ... (-)
+
+part2 input = do 
+    let (listL, listR) = unzip input
+    let listLCounts    = map (getCountFrom listR) listL
+    let score          = getScore listL listLCounts
+    score
+
+getCountFrom     = findCount . countOccurrences
+getScore         = sum ... zipWith (*)
+findCount        = flip (Map.findWithDefault 0)
+countOccurrences = foldr updateCount Map.empty
+updateCount      = flip (Map.insertWith (+)) 1
+
+--------------------------------------------------------------------------------
+-- Problem specific input parsing
+parseText :: String -> Maybe InputData
 parseText = traverse parseLine . lines
 
-parseLine :: String -> Maybe (Int, Int)
-parseLine line =
-    case words line of
-        [x, y] -> do
-            x' <- readMaybe x
-            y' <- readMaybe y
-            return (x', y')
-        _ -> Nothing
+parseLine x = toPair =<< parseLineInt x
+
+toPair [a, b] = Just (a, b)
+toPair _      = Nothing
+
+--------------------------------------------------------------------------------
+-- General supporting functions
+-- TODO: Move to library and import
+--------------------------------------------------------------------------------
+
+-- Useful combinators
+(...) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
+f ... g = \x y -> f (g x y)
+
+runSolution :: Solution -> IO ()
+-- Option 1: Procedural style
+-- runSolution solution = do
+--     (fileName:_)   <- getArgs
+--     text           <- readFile fileName
+--     let parsedText = parseText text
+--     traverse_ solution parsedText
+
+-- Option 2: Almost point-free functional (but more readable)
+runSolution f = traverse_ f . parseFileText =<< readUserFile getArg
+
+-- Option 3: Point-free functional (can't think of a way to make this readable)
+-- runSolution = (readUserFile getArg >>=) . (. parseFileText) . traverse_
+-- runSolution = (readUserFile getArg >>=) . (parseFileText .:. traverse_)
+-- (.:.) :: (a1 -> b) -> (a -> b -> c) -> (a -> a1 -> c)
+-- f .:. g = \x y -> g x (f y)
+
+-- General input parsing utilities
+getArg        = fmap listToMaybe getArgs
+readUserFile  = (traverse readFile =<<)
+parseFileText = (parseText =<<)
+parseLineInt = traverse readMaybe . words
 
